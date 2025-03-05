3ce862f1f5dee2d86d763f6d386026b04ac5268e
Denis Tyukavin <dtyukavin@dmvdesk.com>
2025-03-05 23:37:03 +0300

    edited styles #1

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -1,13 +1,56 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    Fancybox.bind("[data-fancybox]", {
+        // Your custom options
+    });
+
     // Инициализация слайдера на Главной
     (function() {
 
-        const swiper = new Swiper('.swiper', {
+        const sliderClientsElem = document.querySelector('.slider-clients');
+
+        if (!sliderClientsElem) return;
+
+        const swiperClients = new Swiper('.slider-clients', {
             loop: true,
             speed: 400,
-            spaceBetween: 40,
-            slidesPerView: 5,
+            slidesPerView: 1,
+            spaceBetween: 20,
+
+            autoplay: {},
+
+            pagination: {
+                el: '.swiper-pagination',
+                type: 'fraction',
+            },
+
+            navigation: {
+                enabled: true,
+                nextEl: '.slider-clients__arrow.--next',
+                prevEl: '.slider-clients__arrow.--prev',
+            },
+
+            breakpoints: {
+                // when window width is >= 320px
+                840: {
+                    spaceBetween: 40,
+                    slidesPerView: 5,
+                },
+            }
+        });
+    })();
+
+    // Инициализация слайдера в Событии
+    (function() {
+
+        const sliderEventElem = document.querySelector('.slider-event');
+
+        if (!sliderEventElem) return;
+
+        const swiperEvent = new Swiper('.slider-event', {
+            loop: true,
+            speed: 400,
+            slidesPerView: 1,
 
             autoplay: {},
 
