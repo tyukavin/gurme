6ea7420fec79e800c60d953bde374ba45edf7572
Denis Tyukavin <dtyukavin@dmvdesk.com>
2025-03-14 18:49:03 +0300

    deploy settings

--- tasks/deploy.js
+++ tasks/deploy.js
@@ -8,7 +8,7 @@ const connect = ftp.create(ftpSettings);
 
 module.exports = function deploy() {
 	return src(['build/**/*.*', '!build/**/*.map'])
-		.pipe(connect.newer('public_html/'))
-		.pipe(connect.dest('public_html/'))
+		.pipe(connect.newer('www/dtyukavin.ru/gurme'))
+		.pipe(connect.dest('www/dtyukavin.ru/gurme'))
 		.on('end', () => console.log(`Finished deploing ./build to https://${chalk.blueBright(ftpSettings.host)}`))
 }
